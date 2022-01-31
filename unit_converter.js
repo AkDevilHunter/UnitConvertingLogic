b204542a5ed8902de9457f6826a7a8668f36a787
AkDevil Hunter <43783384+AkDevilHunter@users.noreply.github.com>
2022-01-31 12:22:28 +0500

    Add files via upload

---
+++ unit_converter.js
@@ -0,0 +1,29 @@
+function handleChange(e){
+    
+   function getValue(id){
+       return document.getElementById(id).value;
+}
+
+var one_from_unit_equals = getValue("equals_number"); // 1 kilometer is equal to 1000 meter
+
+if(e.target.id == "" || typeof e.target.id === undefined){
+
+   function getValue(id){
+       return document.getElementById(id).value;
+   }
+   function setValue(id,val){
+       return document.getElementById(id).value = val;
+   }
+   if(getValue("from_unit_number") !== ""){
+       return document.getElementById("to_unit_number").value = getValue("from_unit_number") * one_from_unit_equals;
+       // setValue("from_unit_number","");
+   }
+   if(getValue("to_unit_number") !== ""){
+       return document.getElementById("from_unit_number").value = getValue("to_unit_number") / one_from_unit_equals;
+       // setValue("to_unit_number","");
+   }
+
+}
+
+
+}
