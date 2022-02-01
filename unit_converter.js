35e374f04986f5e46ec00c1b247aaf19d7dcf45e
AkDevil Hunter <43783384+AkDevilHunter@users.noreply.github.com>
2022-02-01 12:13:26 +0500

    Update unit_converter.js

--- unit_converter.js
+++ unit_converter.js
@@ -1,29 +1,30 @@
-function handleChange(e){
+function handleChange(){
     
-   function getValue(id){
-       return document.getElementById(id).value;
-}
+    function getValue(id){
+        return document.getElementById(id).value;
+ }
+ 
+ var one_from_unit_equals = getValue("equals_number"); // 1 kilometer is equal to 1000 meter
+ 
 
-var one_from_unit_equals = getValue("equals_number"); // 1 kilometer is equal to 1000 meter
+ 
+    function getValue(id){
+        return document.getElementById(id).value;
+    }
+    function setValue(id,val){
+        return document.getElementById(id).value = val;
+    }
+    if(getValue("from_unit_number") !== ""){
+        return document.getElementById("to_unit_number").value = getValue("from_unit_number") * one_from_unit_equals;
+        // setValue("from_unit_number","");
+    }
+    if(getValue("to_unit_number") !== ""){
+        return document.getElementById("from_unit_number").value = getValue("to_unit_number") / one_from_unit_equals;
+        // setValue("to_unit_number","");
+    }
 
-if(e.target.id == "" || typeof e.target.id === undefined){
+ 
+ 
+ }
 
-   function getValue(id){
-       return document.getElementById(id).value;
-   }
-   function setValue(id,val){
-       return document.getElementById(id).value = val;
-   }
-   if(getValue("from_unit_number") !== ""){
-       return document.getElementById("to_unit_number").value = getValue("from_unit_number") * one_from_unit_equals;
-       // setValue("from_unit_number","");
-   }
-   if(getValue("to_unit_number") !== ""){
-       return document.getElementById("from_unit_number").value = getValue("to_unit_number") / one_from_unit_equals;
-       // setValue("to_unit_number","");
-   }
-
-}
-
-
-}
+ document.getElementById("submitForm").addEventListener("click", handleChange);
